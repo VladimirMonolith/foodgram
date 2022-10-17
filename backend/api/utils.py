7153efc72784cb62c91dca270b6eec457620f13f
Гут Владимир <vladimir.monolith@yandex.ru>
2022-10-17 19:41:16 +0300

    Добавлен шрифт 2

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -47,13 +47,18 @@ from reportlab.pdfgen import canvas
 
 def create_shopping_list(ingredients_cart):
     """Функция для формирования списка покупок."""
-    font = 'TNR'
-    pdfmetrics.registerFont(TTFont('TNR', 'data/times.ttf', 'UTF-8'))
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = (
+        'attachment; filename="shopping_cart.pdf"'
+    )
+    pdfmetrics.registerFont(
+        TTFont('TNR', 'data/Times-New-Romance.ttf', 'UTF-8')
+    )
     buffer = io.BytesIO()
     pdf_file = canvas.Canvas(buffer)
-    pdf_file.setFont(font, 24)
+    pdf_file.setFont('TNR', 24)
     pdf_file.drawString(200, 800, 'Список покупок.')
-    pdf_file.setFont(font, 14)
+    pdf_file.setFont('TNR', 14)
     from_bottom = 750
     for number, ingredient in enumerate(ingredients_cart, start=1):
         pdf_file.drawString(
@@ -67,10 +72,10 @@ def create_shopping_list(ingredients_cart):
         if from_bottom <= 50:
             from_bottom = 800
             pdf_file.showPage()
-            pdf_file.setFont(font, 14)
+            pdf_file.setFont('TNR', 14)
     pdf_file.showPage()
     pdf_file.save()
-    buffer.seek(0)
-    return HttpResponse(
-        buffer, as_attachment=True, filename='shopping_list.pdf'
-    )
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
